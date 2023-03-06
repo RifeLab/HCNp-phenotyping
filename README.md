0fcbc079bfc8d073c2794ec04802f0a1bc68c719
chaneylc <chaneylc@gmail.com>
2023-03-06 14:30:43 -0600

    init commit for hcn

--- README.md
+++ README.md
@@ -1,4 +1,81 @@
-# Project Template
+# HCN Phenotyping
 
-1. Create a [new repository](https://github.com/organizations/FieldPheno/repositories/new) in FieldPheno and use this repository as a template
-2. Add the data, scripts, explanations, etc. that will enable your specific project to be reproducible. An outside person should be able to recreate your specific project from this repository.
+## Overview
+
+A python script for measuring the 'blueness' of a circular sample. Users can define a static grid with the ccpa2_grid_define.py script, here the user left-clicks the center on each well from left-to-right, top-to-bottom. This will print a list of coordinates in the console, these can be copied and pasted into the ccpa_grid.py script to be used for bulk measurements. 
+
+The scoring uses LAB thresholding to find dark and blue pixels.
+
+```
+python ccpa2_grid.py /full/path/to/example_image
+```
+
+## Definer
+
+Renders the user's mouse position, the console prints a list of clicked points.
+
+<p align="center">
+	<img src="./readme_assets/definer.png" width="1920" height="1080" />
+</p>
+
+## Main script
+
+Renders the static coordinates created by the definer. On left click, the 'blueness' for each well is scored, and the tabular results are copied to the os clipboard. Image can be rotated with 1/2, or cropped with the space button. There is also a circles script that renders/analyzes circles.
+
+<p align="center">
+	<img src="./readme_assets/example.png" width="1920" height="1080"	/>
+</p>
+
+
+## Example output
+```
+Pixel Blueness Scoring
+10
+3
+33
+38
+47
+46
+64
+44
+21
+10
+9
+3
+17
+4
+26
+10
+51
+13
+48
+62
+43
+53
+45
+48
+16
+1
+4
+53
+59
+65
+58
+13
+7
+11
+35
+25
+1
+34
+8
+44
+45
+47
+54
+70
+17
+44
+59
+5
+```
---
+++ scripts/ccpa2_grid_define.py
@@ -0,0 +1,279 @@
+import numpy as np
+import cv2
+import sys
+import random as rng
+import pyperclip
+from matplotlib import pyplot as plt
+
+
+###
+###
+###notes first image assay (image(1).png) is in BGR format!
+###
+###
+
+STATIC_GRID_DOT_LOCATIONS = [(353, 861), (407, 860), (456, 860), (506, 862), (564, 863), (617, 864), (671, 860), (726, 863), (776, 863), (832, 862), (883, 864), (939, 865), (350, 964), (405, 966), (456, 966), (510, 967), (565, 966), (616, 968), (669, 967), (723, 966), (778, 969), (828, 967), (882, 969), (936, 967), (353, 1074), (407, 1076), (456, 1074), (511, 1075), (563, 1075), (618, 1077), (672, 1077), (723, 1077), (777, 1074), (830, 1073), (884, 1075), (938, 1076), (352, 1180), (407, 1175), (458, 1178), (510, 1178), (564, 1179), (618, 1179), (670, 1178), (724, 1180), (774, 1179), (829, 1181), (884, 1179), (937, 1178)]
+
+#used to create ROI
+GRID_BOX_SPACING=21
+
+#(row, column) grid size in this case A-H and 1-12
+GRID_SIZE = (8, 12)
+
+_paused = False
+_click_coordinates = list()
+_mouse_coordinates = None
+_mouse_scroll_radius = 12
+_rotate_angle = 0.0
+_left_click_is_pressed = False
+_blue_memory = [0 for x in STATIC_GRID_DOT_LOCATIONS]
+
+def rotate_image(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
+
+def crop(img, x, y, w, h):
+    
+    return img[y:y+h, x:x+w]
+
+def distance(a, b): 
+    
+    return int(((a[0]-b[0])**2 + (a[1]-b[1])**2)**0.5)
+
+def calculate_blue_hsv(frame, lab, top_left, bottom_right):
+    
+    try:
+    
+        #get key press to check if we want to plot B distribution
+#        key_press = cv2.waitKey(1) & 0xFF
+
+        y1 = top_left[1]
+
+        x1 = top_left[0]
+
+        y2 = bottom_right[1]
+
+        x2 = bottom_right[0]
+        
+        roi = lab[y1:y2, x1:x2]
+        #print(roi.shape)
+        #calculate length and width
+        length = abs(x2 - x1)
+        width = abs(y2 - y1)
+        
+        #calculate total area of roi
+        area = length*width
+        
+        #uncomment to see the individual rois
+        #cv2.imshow("dots", roi)
+
+        #lumin, green->yellow, blue->yellow
+#        lower_range = np.array([0, 0, 0])
+#        upper_range = np.array([255, 255, 128])
+#        mask = cv2.inRange(roi, lower_range, upper_range)
+#               
+        a = np.argwhere(roi[:,:,2] >= 0)
+        b = np.argwhere(roi[:,:,2] <= 128)
+        c = np.argwhere(roi[:,:,0] > 128)
+        d = np.argwhere(roi[:,:,0] < 255)
+
+        aset = set([tuple(x) for x in a])
+        bset = set([tuple(x) for x in b])
+        cset = set([tuple(x) for x in c])
+        dset = set([tuple(x) for x in d])
+        
+        a = np.array([x for x in aset & bset & cset & dset])
+        
+        score = 0
+#        for i in range(0,width):
+#            for j in range(0,length):
+#                pixel = roi[i][j]
+#                #check if Luminosity is between 128-255
+#                #check if B value is between 0-128
+#                if ((pixel[2] >= 0) and (pixel[2] <= 128)) and (pixel[0] > 128) and (pixel[0] < 255):
+#                    score = score + (255-pixel[0])
+        
+        for (x,y) in a:
+            pixel = roi[x][y]
+            score = score + (255-pixel[0])
+            
+        #uncomment to see the masked calculation area
+        #cv2.imshow("dots", mask)
+        
+        count = int(score / area)
+        #count all blue pixels and normalize by area of roi
+        #count = int((np.count_nonzero(mask) / area) * 255)
+        
+#        if key_press == ord('r'):
+#            full_mask = cv2.inRange(lab, lower_range, upper_range)
+#            cv2.imshow("lab", full_mask)
+#            
+#        if key_press == ord('e'):
+#            #histogram of pixel intensity values
+#            plt.hist(roi[:,:, 2].ravel(), 256, [0,256])
+#            plt.show()
+            
+        return count
+    
+    except Exception as e:
+        
+        print(e)
+        
+        return 0
+    
+def click_callback(event, x, y, flags, param):
+    
+    global _left_click_is_pressed
+    global _rotate_angle
+    global _click_coordinates
+    global _mouse_coordinates
+    global _mouse_scroll_radius
+    global frame
+    
+    #get a key pressed during a mouse event
+    key = cv2.waitKey(1) & 0xFF
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+        
+        _left_click_is_pressed = True
+        
+        _click_coordinates.append((x,y))
+        
+        print(_click_coordinates)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        
+        _left_click_is_pressed = False
+        
+    _mouse_coordinates = (x,y)
+
+def build_grid(mouse_coordinates, grid):
+    
+    #make list of dot locations relative to mouse coordinates using the param grid spacing
+    local_coords = []
+    delta_x = 0
+    delta_y = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    size = rows*cols
+    for index, dot in enumerate(grid):
+        
+        if index == 0:
+            
+            local_coords.append(mouse_coordinates)
+            
+    
+    return local_coords
+
+def render_dynamic(frame, lab, coords, grid):
+    
+    global _blue_memory
+    
+    frame = rotate_image(frame, _rotate_angle)
+    lab = rotate_image(lab, _rotate_angle)
+    
+    layer = None
+    
+    local_coords = build_grid(coords, STATIC_GRID_DOT_LOCATIONS)
+    
+    #draw the roi for each coordinate in the static grid
+    #also use calculate_blue to analyze the amount of blue in the ROI
+    for index, coordinate in enumerate(local_coords):
+            
+        top_left = (coordinate[0]-GRID_BOX_SPACING, coordinate[1]-GRID_BOX_SPACING)
+        bottom_right = (coordinate[0]+GRID_BOX_SPACING, coordinate[1]+GRID_BOX_SPACING)
+
+        #first is top left, second is bottom right corners
+        cv2.rectangle(frame, top_left, bottom_right, (0,255,0), 1)
+        
+    return frame
+
+windowName = "CCP Assay {}".format(sys.argv[1])
+cv2.namedWindow(windowName)
+cv2.setMouseCallback(windowName, click_callback)
+
+file_name = sys.argv[1]
+
+print("Loading " + file_name)
+
+mouseLabel = (0,255,255)
+hullLabel = (0,0,255)
+bbox = None
+
+frame = cv2.imread(file_name)
+
+print("Size " + str(frame.shape[0]) + " " + str(frame.shape[1]))
+
+frame = cv2.pyrDown(frame)
+frame = cv2.pyrDown(frame)
+frame = cv2.pyrDown(frame)
+
+print("Size " + str(frame.shape[0]) + " " + str(frame.shape[1]))
+
+while cv2.getWindowProperty(windowName, cv2.WND_PROP_VISIBLE) >= 1:
+        
+    #convert color space to LAB
+    lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        
+            cv2.destroyAllWindows()
+            
+            break
+    
+    while cv2.getWindowProperty(windowName, cv2.WND_PROP_VISIBLE) >= 1:
+
+        key_press = cv2.waitKey(1) & 0xFF
+        
+        if key_press == ord(' '):
+            
+            _paused = not _paused
+            
+        elif key_press in [ord('q'), 27]:
+            
+            cv2.destroyAllWindows()
+
+            break
+
+        elif key_press == ord('1'):
+            
+            _rotate_angle = _rotate_angle - 1.0
+        
+        elif key_press == ord('2'):
+            
+            _rotate_angle = _rotate_angle + 1.0
+            
+        if _paused:
+            
+            bbox = cv2.selectROI("frame", frame, False, False)
+            
+            _paused = not _paused
+            
+        if not _paused:
+
+            if frame is None:
+                
+                break
+                
+            if bbox is not None:
+                
+                frame = crop(frame, *bbox)
+                
+                bbox = None
+                
+                print("Cropped!")
+                
+                print(frame.shape)
+                
+            #render clicks using addWeighted to combine layers
+            draw_layer = frame.copy()
+            
+            if _mouse_coordinates:
+            
+                draw_layer = render_dynamic(draw_layer, lab, _mouse_coordinates, STATIC_GRID_DOT_LOCATIONS)
+                                                                                      
+            cv2.imshow(windowName, draw_layer)
+
+cv2.destroyAllWindows()
